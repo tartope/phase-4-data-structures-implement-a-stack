5b142f898dc8af3f0fc8d230fb44f899b8b9e417
Tunisia Artope <artope@yahoo.com>
2022-05-30 10:45:53 -0400

    comments added

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,18 +1,22 @@
 # your code here
+# First, create the Stack class and set it up to initialize an instance variable @stack that points to an empty array.
 class Stack
 
     def initialize
         @stack = []
     end
 
+    # Stack#push(value): add an element to the top of the stack
     def push(value)
         @stack.push(value)
     end
 
+    # Stack#pop: remove the element at the top of the stack
     def pop
         @stack.pop
     end
 
+    # Stack#peek: return the value of the element at the top of the stack without removing it
     def peek
         @stack.last    #? why not @stack.peek
     end
